a5baac32a4ed54a9e7df057e698f77f2c931f1eb
Andrew Marrone <github@andrewmarrone.com>
2012-02-08 18:47:23 -0800

    using marky_markov for sentence generation

--- bot.rb
+++ bot.rb
@@ -1,14 +1,12 @@
+require 'marky_markov'
 require_relative 'FaceDriver.rb'
 
 if __FILE__ == $0
 	connect = FaceDriver.new
 	connect.login(ARGV[0], ARGV[1])
-
-	data = PersistentDictionary
-	data.load("dict/wall.json")
-	result = TwoSentenceGen.new(data.dictionary)
-	message = result.generate(8)
-
+	markov = MarkyMarkov::Dictionary.new('facebook')
+	markov.parse_file "wall.txt"
+	message = markov.generate_n_sentences 3
 	connect.post(message)
 	connect.logout
 	connect.close
